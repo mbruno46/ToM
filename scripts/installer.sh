30fa30316ad37a0915896f827d58bc530f3cd3b1
Mattia Bruno <38449948+mbruno46@users.noreply.github.com>
2022-01-16 00:17:07 +0100

    added arm64 zip for M1 macbook

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -2,17 +2,28 @@
 
 EXT=$1
 
-OS=`uname`
+if [[ "$EXT" == "zip" ]]; then
+  if [[ `uname -m` == "x86_64" ]]; then
+    ARCH='x64'
+  elif [[ `uname -m` == "arm64" ]]; then
+    ARCH="arm64"
+  fi
+elif [[ "$EXT" == "deb" ]]; then
+  ARCH='amd64'
+elif [[ "$EXT" == "rpm" ]]; then
+  ARCH='x86_64'
+fi
+
 CURL=$(curl -k -s "https://api.github.com/repos/mbruno46/ToM/releases/latest")
 
 LATEST_VERSION=$(echo $CURL | sed -e 's/.*"tag_name": "v\([^"]*\)".*/\1/')
 echo "Latest version $LATEST_VERSION"
 
-SED=`echo 's/.*"browser_download_url": "\([^"]*\).'${EXT}'".*/\1/'`
-URL=$(echo $CURL | sed -e "${SED}").$EXT
+SED=`echo 's/.*"browser_download_url": "\([^"]*\)'${ARCH}'.'${EXT}'".*/\1/'`
+URL=$(echo $CURL | sed -e "${SED}")${ARCH}.$EXT
 
-SED=`echo 's/.*"name": "\([^"]*\).'${EXT}'".*/\1/'`
-NAME=$(echo $CURL | sed -e "${SED}").${EXT}
+SED=`echo 's/.*"name": "\([^"]*\)'${ARCH}'.'${EXT}'".*/\1/'`
+NAME=$(echo $CURL | sed -e "${SED}")${ARCH}.${EXT}
 
 cd /tmp
 curl -L -O -k $URL
